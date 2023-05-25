f1d1807eed4f294539b9b00b50238e88f60012b0
Sami Laubo <slaubo99@gmail.com>
2023-05-25 09:09:47 +0200

    Update train_panoptic_bev_nuscenes.sh

--- scripts/train_panoptic_bev_nuscenes.sh
+++ scripts/train_panoptic_bev_nuscenes.sh
@@ -2,7 +2,7 @@ module load gcc/8.4.0-cuda python/3.7.7 cuda/11.1.1
 source ../../venv/panoptic_bev/bin/activate
 
 CUDA_VISIBLE_DEVICES=0,1 \
-python3 -m torch.distributed.launch --nproc_per_node=2 --master_addr=172.19.19.51 --master_port=37599 train_panoptic_bev.py \
+../../venv/panoptic_bev/bin/python -m torch.distributed.launch --nproc_per_node=2 --master_addr=172.19.19.51 --master_port=37599 train_panoptic_bev.py \
                                     --run_name=run1 \
                                     --project_root_dir=/home/laubo/DLAV/PanopticBEV \
                                     --seam_root_dir=/home/laubo/DLAV/nuscenes/nuScenes_panopticbev\
